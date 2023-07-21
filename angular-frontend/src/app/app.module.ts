06018c4b3e49be3b5d4923a37a9a137dcf0d54ed
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-07-21 19:01:53 +0530

    user constant created

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentModuleModule } from './components/component-module/component-module.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentModuleModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ComponentModuleModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      newestOnTop: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
